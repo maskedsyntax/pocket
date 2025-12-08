813f00f7bf7a428375f3827aee74d5af2223d1db
maskedsyntax <aftaab@aftaab.xyz>
2025-12-08 21:09:47 +0530

    chore: update build script

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@ echo "Setting up Pocket launcher..."
 # Build project
 echo "Building project..."
 mkdir -p bin
-make clean
-make
+
+go build -o bin/pocket ./cmd/pocket
+
 
 # Confirm build
 if [ -f bin/pocket ]; then
     echo "Build successful!"
+    cp ./bin/pocket ~/.local/bin/pocket
 else
     echo "Build failed!"
     exit 1
